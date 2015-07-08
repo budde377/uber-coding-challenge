23ec84086d847ff24404c4ca81c97c89d612b128
Christian Budde Christensen <budde377@gmail.com>
2015-07-08 18:07:23 +0200

    Changed url

--- departure_server/__main__.py
+++ departure_server/__main__.py
@@ -9,7 +9,7 @@ import http.server
 
 if __name__ == "__main__":
     base_url = sys.argv[1]
-    server = http.server.HTTPServer(('127.0.0.1', 8000),
+    server = http.server.HTTPServer(('', 8000),
                                     departure_server.request_handler.setup_handler(
                                         RejseplanenQueryStrategy(base_url)))
     server.serve_forever()
